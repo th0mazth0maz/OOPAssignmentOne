2b51fe184d3fd1c821855a5a9c896d9852ea93ca
Tom Smith <124048238+th0mazth0maz@users.noreply.github.com>
2023-03-08 21:23:49 +0000

    Add files via upload

---
+++ Updated CMP1903M A01 2223/CMP1903M A01 2223/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Program
+    {
+        //'Main' is the program start point. Class is only used here to call the 'TestEverything' method located in the 'Test.cs' class. 
+        static void Main(string[] args)
+        {
+            Test.TestEverything();
+        }
+    }
+}
